c8c38496fd1fbf01de2827156cdc23849779645c
赵玉川 <botue@sina.com>
2021-11-26 11:49:04 +0800

    更新「网易云音乐」gulp 版本

--- music/gulpfile.js
+++ music/gulpfile.js
@@ -1,28 +1,31 @@
-const gulp = require('gulp');
+
+const {src, dest, series, parallel, watch} = require('gulp');
+
+// const less = require('gulp-less');
+const sass = require('gulp-sass')(require('sass'));
+
+const postcss = require('gulp-postcss');
+
 const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
 const reload = browserSync.reload;
 
-// scss编译后的css将注入到浏览器里实现更新
-gulp.task('sass', function() {
-  return gulp.src("./scss/*.scss")
-      .pipe(sass({
-          outputStyle: 'expanded'
-      }))
-      .pipe(gulp.dest("./css"))
-      .pipe(reload({stream: true}));
-});
 
-function serve() {
-  browserSync.init({
-    server: './'
-  });
-
-  gulp.watch('./scss/*.scss', gulp.series('sass'));
-  gulp.watch('./*.html').on('change', reload);
+function css() {
+  return src('scss/**/[!_]*.scss')
+    .pipe(sass())
+    .pipe(postcss([]))
+    .pipe(dest('./css'));
 }
 
-exports.default = serve;
+function serve(cb) {
+  browserSync.init({
+    server: "./"
+  });
 
+  watch('./scss/*.scss', series(css)).on('change', reload);
+  watch('./*.html').on('change', reload);
 
+  cb();
+}
 
+exports.default = series(serve, css);
